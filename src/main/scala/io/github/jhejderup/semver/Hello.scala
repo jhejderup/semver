4596b970957ddfe568282b8434209f2b31682757
Joseph Hejderup <jhejderup@users.noreply.github.com>
2017-06-29 20:43:20 +0200

    minimal working node-semver port :)

--- src/main/scala/io/github/jhejderup/semver/Hello.scala
+++ src/main/scala/io/github/jhejderup/semver/Hello.scala
@@ -7,9 +7,12 @@ import io.github.jhejderup.semver.parser.Semver
 object Hello extends App {
 
 
-  val range = SemverRangeCompiler("<1.2.3").right.get.transform()
-  val ver = SemverRangeCompiler("1.2.3").right.get
+  val range = SemverRangeCompiler("<=1.2.3").right.get.transform()
+  val ver = SemverRangeCompiler("1.2.3-beta").right.get //2.4.3-alpha 1.2.3-pre.2
     .asInstanceOf[Semver]
+
+
+  // <=2.4.3+asdf := <=2.4.3    2.4.3-alpha
 
 
   println(range.evaluate(ver))
--- src/test/scala/io/github/jhejderup/semver/SemverRangeCompilerSpec.scala
+++ src/test/scala/io/github/jhejderup/semver/SemverRangeCompilerSpec.scala
@@ -1,6 +1,6 @@
 package io.github.jhejderup.semver.compiler
 
-import io.github.jhejderup.semver.parser.Semver
+import io.github.jhejderup.semver.parser.{Semver, SemverAST}
 import org.scalatest._
 
 class SemverRangeCompilerSpec extends FlatSpec with Matchers {
@@ -206,9 +206,9 @@ class SemverRangeCompilerSpec extends FlatSpec with Matchers {
       ("^1.2.3+build", "1.2.3"),
       ("^1.2.3+build", "1.3.0"),
       ("1.2.3-pre+asdf - 2.4.3-pre+asdf", "1.2.3"),
-   //   ("1.2.3pre+asdf - 2.4.3-pre+asdf", "1.2.3", true),
-   //   ("1.2.3-pre+asdf - 2.4.3pre+asdf", "1.2.3", true),
-   //   ("1.2.3pre+asdf - 2.4.3pre+asdf", "1.2.3", true),
+   //   ("1.2.3pre+asdf - 2.4.3-pre+asdf", "1.2.3", true),  --- this for loose case, not supported yet
+   //   ("1.2.3-pre+asdf - 2.4.3pre+asdf", "1.2.3", true),  --- this for loose case, not supported yet
+   //   ("1.2.3pre+asdf - 2.4.3pre+asdf", "1.2.3", true),  --- this for loose case, not supported yet
       ("1.2.3-pre+asdf - 2.4.3-pre+asdf", "1.2.3-pre.2"),
       ("1.2.3-pre+asdf - 2.4.3-pre+asdf", "2.4.3-alpha"),
       ("1.2.3+asdf - 2.4.3+asdf", "1.2.3"),
@@ -324,9 +324,9 @@ class SemverRangeCompilerSpec extends FlatSpec with Matchers {
       ("^1.2.3", "1.2.3-beta"),
       ("=0.7.x", "0.7.0-asdf"),
       (">=0.7.x", "0.7.0-asdf"),
-//  //    ("1", "1.0.0beta", true),
-//  //    ("<1", "1.0.0beta", true),
-//  //    ("< 1", "1.0.0beta", true),
+//  //    ("1", "1.0.0beta", true),  --- this for loose case, not supported yet
+//  //    ("<1", "1.0.0beta", true),  --- this for loose case, not supported yet
+//  //    ("< 1", "1.0.0beta", true),  --- this for loose case, not supported yet
       ("1.0.0", "1.0.1"),
       (">=1.0.0", "0.0.0"),
       (">=1.0.0", "0.0.1"),
@@ -338,7 +338,7 @@ class SemverRangeCompilerSpec extends FlatSpec with Matchers {
       ("<=2.0.0", "2.2.9"),
       ("<2.0.0", "2.9999.9999"),
       ("<2.0.0", "2.2.9"),
-//   //   (">=0.1.97", "v0.1.93", true),
+      (">=0.1.97", "v0.1.93"/*, true */),
       (">=0.1.97", "0.1.93"),
       ("0.1.20 || 1.2.4", "1.2.3"),
       (">=0.2.3 || <0.0.1", "0.0.3"),
@@ -364,7 +364,7 @@ class SemverRangeCompilerSpec extends FlatSpec with Matchers {
       ("~1.0", "1.1.0"), // >=1.0.0 <1.1.0
       ("<1", "1.0.0"),
       (">=1.2", "1.1.1"),
-//    //  ("1", "2.0.0beta", true),
+//    //  ("1", "2.0.0beta", true),  --- this for loose case, not supported yet
       ("~v0.5.4-beta", "0.5.4-alpha"),
       ("=0.7.x", "0.8.2"),
       (">=0.7.x", "0.6.2"),
@@ -394,4 +394,75 @@ class SemverRangeCompilerSpec extends FlatSpec with Matchers {
     res.reduceOption(_ || _).getOrElse(true) should be(false)
   }
 
+  /* -->> need to have some better handling for this
+  test('\ninvalid version numbers', function(t) {
+  ['1.2.3.4',
+   'NOT VALID',
+   1.2,
+   null,
+   'Infinity.NaN.Infinity'
+  ].forEach(function(v) {
+    t.throws(function() {
+      new SemVer(v);
+    }, {name:'TypeError', message:'Invalid Version: ' + v});
+  });
+
+  t.end();
+});
+  * */
+
+
+  "The Semver object maxSatisying" should "equal true" in {
+    val t1 = new Tuple3[List[String], String, String](List[String]("1.2.3", "1.2.4"), "1.2", "1.2.4")
+    val t2 = new Tuple3[List[String], String, String](List[String]("1.2.4", "1.2.3"), "1.2", "1.2.4")
+    val t3 = new Tuple3[List[String], String, String](List[String]("1.2.3", "1.2.4", "1.2.5", "1.2.6"), "~1.2.3", "1.2.6")
+    val t4 = new Tuple3[List[String], String, String](List[String]("1.1.0", "1.2.0", "1.2.1", "1.3.0", "2.0.0-b1", "2.0.0-b2", "2.0.0-b3", "2.0.0", "2.1.0"), "~2.0.0", "2.0.0") //change later to original
+    //t4 should be [["1.1.0", "1.2.0", "1.2.1", "1.3.0", "2.0.0-b1", "2.0.0-b2", "2.0.0-b3", "2.0.0", "2.1.0"], '~2.0.0', '2.0.0', true]
+
+
+    def semver(str: String): Semver = SemverRangeCompiler(str).right.get.asInstanceOf[Semver]
+    def range(str: String): SemverAST =  SemverRangeCompiler(str).right.get.transform()
+
+    val res = for {
+      cmp <- List(t1,t2,t3, t4)
+    } yield {
+      val r = range(cmp._2)
+      val f = semver(cmp._3)
+      val max = cmp._1.map(v => semver(v)).filter(v => r.evaluate(v)).max
+      max == f
+
+    }
+
+
+    res.reduceOption(_ && _).getOrElse(false) should be(true)
+
+
+  }
+
+  "The Semver object minSatisying" should "equal true" in {
+    val t1 = new Tuple3[List[String], String, String](List[String]("1.2.3", "1.2.4"), "1.2", "1.2.3")
+    val t2 = new Tuple3[List[String], String, String](List[String]("1.2.4", "1.2.3"), "1.2", "1.2.3")
+    val t3 = new Tuple3[List[String], String, String](List[String]("1.2.3", "1.2.4", "1.2.5", "1.2.6"), "~1.2.3", "1.2.3")
+    val t4 = new Tuple3[List[String], String, String](List[String]("1.1.0", "1.2.0", "1.2.1", "1.3.0", "2.0.0-b1", "2.0.0-b2", "2.0.0-b3", "2.0.0", "2.1.0"), "~2.0.0", "2.0.0") //change later to original
+    //t4 should be [["1.1.0", "1.2.0", "1.2.1", "1.3.0", "2.0.0-b1", "2.0.0-b2", "2.0.0-b3", "2.0.0", "2.1.0"], '~2.0.0', '2.0.0', true]
+
+
+    def semver(str: String): Semver = SemverRangeCompiler(str).right.get.asInstanceOf[Semver]
+    def range(str: String): SemverAST =  SemverRangeCompiler(str).right.get.transform()
+
+    val res = for {
+      cmp <- List(t1,t2,t3, t4)
+    } yield {
+      val r = range(cmp._2)
+      val f = semver(cmp._3)
+      val min = cmp._1.map(v => semver(v)).filter(v => r.evaluate(v)).min
+
+      min == f
+
+    }
+
+    res.reduceOption(_ && _).getOrElse(false) should be(true)
+
+
+  }
 }
